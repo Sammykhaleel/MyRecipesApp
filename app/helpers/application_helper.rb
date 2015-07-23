254b4ea7571c7513165d45d4b9dfcc3f51a50e39
sammykhaleel <sammykhaleel83@gmail.com>
2015-07-23 14:00:00 -0400

    forms and partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def gravatar_for(chef, options = {size: 80})
+    gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
+    size = options[:size]
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
+  end
+  # def avatar_url(chef)
+  #   gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
+  #   "http://gravatar.com/avatar/#{gravatar_id}.png"
+  # end
 end
